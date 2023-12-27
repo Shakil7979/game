6e6fd203ca01709696aaa8184bf66b7abb73b0e4
Sadbin Shakil <sadbin7979@gmail.com>
2023-12-28 00:45:41 +0600

    upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-	// Mobile Menu
-	// $('.logo a i').click(function(){
-	// 	$('.menu ul').slideToggle(1000);
+$(document).ready(function() {
+    // Mobile Menu
+    // $('.logo a i').click(function(){
+    // 	$('.menu ul').slideToggle(1000);
 
-	// 	return false
-	// });
+    // 	return false
+    // });
 
-	// Banner Carousel
-	// $('.banner_carousel').owlCarousel({
-	// 	items:1,
-	// 	loop:true,
-	// 	nav:true,
-	// 	dots:true,
-	// 	autoplay:true,
-	// });
+    // Banner Carousel
+    // $('.banner_carousel').owlCarousel({
+    // 	items:1,
+    // 	loop:true,
+    // 	nav:true,
+    // 	dots:true,
+    // 	autoplay:true,
+    // });
 
 
 
