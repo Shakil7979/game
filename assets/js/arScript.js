e6aa9064f37a97ee093f111dae73693d92969ef4
Abdur Rahim <abdurrahim300431@gmail.com>
2023-12-31 15:31:22 +0600

    tab update

--- assets/js/arScript.js
+++ assets/js/arScript.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
     $(".ar_header_bars a.arMobileBars2").click(function(){
         $(".ar_header_navbar").slideToggle();
       });
+
+
+      $(".ar_chatlist_btn.list").click(function(){
+        $(".ar_tabTwo").addClass("twotwo");
+      });
+      $(".ar_chatlist_btn.all").click(function(){
+        $(".ar_tabOne").addClass("oneone");
+      });
+
+      $(".ar_chatlist_btn.list").click(function(){
+        $(".ar_tabOne").removeClass("oneone");
+      });
+
+      $(".ar_chatlist_btn.all").click(function(){
+        $(".ar_tabTwo").removeClass("twotwo");
+      });
+
+      
 })
