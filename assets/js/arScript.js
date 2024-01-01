2b00ecb7818aec6c85e4002ef6cf5dfcedd922f2
Abdur Rahim <abdurrahim300431@gmail.com>
2024-01-01 19:52:59 +0600

    last page update

--- assets/js/arScript.js
+++ assets/js/arScript.js
@@ -19,5 +19,29 @@ $(document).ready(function(){
         $(".ar_tabTwo").removeClass("twotwo");
       });
 
+
+      $('.ar_town_carousel_wrapper').owlCarousel({
+        items:4,
+        dots:false,
+        loop:true,
+        margin:20,
+        nav:true,
+        navText:["<img src='assets/images/town/leftarrow.png'>","<img src='assets/images/town/rightarrow.png'>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            384:{
+              items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
       
 })
