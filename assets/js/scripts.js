50c81089e4b324302d0f7f6914b103dd0ebb2f3e
Sadbin Shakil <sadbin7979@gmail.com>
2024-01-01 10:44:59 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,16 +2,18 @@ $(document).ready(function() {
     // Mobile Menu
     $('.menu_close').click(function(){
     	$('.menu_expression').hide(); 
+
+        return false;
+    });
+    $('.modal_close').click(function(){
+    	$('.modal_area').hide(); 
+
+        return false;
     });
 
-    // Banner Carousel
-    // $('.banner_carousel').owlCarousel({
-    // 	items:1,
-    // 	loop:true,
-    // 	nav:true,
-    // 	dots:true,
-    // 	autoplay:true,
-    // });
+
+    
+
 
 
 
